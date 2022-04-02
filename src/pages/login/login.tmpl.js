d217a07cde6e33d6ecb1ed82ee2aa4bd6875cb1a
A. Morozov <amorozov@t1-consulting.ru>
2022-04-02 16:19:16 +0300

    completed index.html

---
+++ src/pages/login/login.tmpl.js
@@ -0,0 +1,32 @@
+export const template = `
+    <main>
+      <section class="login-page__content">
+        <h1 class="login-page__title">Вход</h1>
+        <form class="login-page__form">
+          <label for="login" class="login-page__login">
+            <p class="input__label">Логин</p>
+            <input type="text" name="login" id="login" class="input__control" />
+          </label>
+          <label for="password" class="login-page__password">
+            <p class="input__label">Пароль</p>
+            <input
+              type="password"
+              id="password"
+              class="input__control"
+            />
+          </label>
+          <a
+            href="./main.html"
+            class="login-page__submit"
+            >
+            Авторизоваться
+          </a>
+        </form>
+        <a
+          href="./sign-up.html"
+          class="login-page__signup"
+          >Нет аккаунта?</a
+        >
+      </section>
+    </main>
+`;
