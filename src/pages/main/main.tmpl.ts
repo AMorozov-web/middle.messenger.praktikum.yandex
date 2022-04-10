6d3164324a3e1462175f0f023912594ebbafe7a2
Alex Morozov <alex.morozov.webdev@gmail.com>
2022-04-10 22:09:39 +0300

    completed main page

---
+++ src/pages/main/main.tmpl.ts
@@ -0,0 +1,37 @@
+export const template = `
+<main>
+  <section class="main-page">
+    <h1 class="visually-hidden">Чаты</h1>
+    <aside class="main-page__inner-wrapper">
+      <div class="main-page__buttons">
+        {{addChatLink}}
+        {{profileLink}}
+      </div>
+<!--      Будет реализовано позже -->
+<!--      <label for="search" class="main-page__search">-->
+<!--        <p class="input__label">Поиск</p>-->
+<!--        <input type="text" name="search" id="search" class="input" />-->
+<!--      </label>-->
+      {{chatsList}}
+    </aside>
+    <div class="main-page__content">
+      <div class="main-page__user-info">
+        <div class="main-page__user-avatar"></div>
+        <p class="main-page__user-name">Пользователь</p>
+        <button class="main-page__user-button" type="button"></button>
+      </div>
+      <ul class="main-page__selected-chat">
+        <li class="main-page__day">
+          <p class="main-page__day-title">19 мая</p>
+          {{messagesList}}
+        </li>
+      </ul>
+<!--      Будет реализовано позже -->
+<!--        <button class="main-page__add-file" type="button">-->
+<!--          <span class="main-page__icon"></span>-->
+<!--        </button>-->
+      {{form}}
+    </div>
+  </section>
+</main>
+`;
---
+++ src/pages/main/main.ts
@@ -0,0 +1,117 @@
+import {template} from './main.tmpl';
+import {Avatar, Block, Button, ChatItem, Form, Input, Link, List, Message} from '../../components';
+import {onFormSubmit, renderDOM} from '../../utils';
+import {BUTTON_TYPE, TAG_NAME} from '../../constants';
+
+type Props = {
+  addChatLink: Link;
+  profileLink: Link;
+  chatsList: List;
+  messagesList: List;
+  form: Form;
+};
+
+const addChatLink = new Link({
+  href: '',
+  text: 'Добавить чат',
+});
+
+const profileLink = new Link({
+  href: './profile.html',
+  text: 'Профиль',
+});
+
+const chatsListData = new Array(3).fill('').map(
+  () =>
+    new ChatItem({
+      avatar: new Avatar({
+        wrapperClassName: 'main-page__chat-avatar',
+      }),
+      lastMessage: 'Последнее сообщение...',
+      newMessagesCount: '1',
+      userName: 'Пользователь',
+      time: '12:00',
+    }),
+);
+
+const chatsList = new List({
+  className: 'main-page__chats-list',
+  items: chatsListData,
+});
+
+const messageData = new Array(4).fill('').map((_, i) => {
+  const className = i % 2 !== 0 ? 'main-page__message--self' : '';
+
+  return new Message({
+    className,
+    text: 'Круто!',
+    time: '12:00',
+  });
+});
+
+const messagesList = new List({
+  className: 'main-page__messages',
+  items: messageData,
+});
+
+const newMessageInput = new Input({
+  className: 'main-page__new-message',
+  id: 'new-message',
+  name: 'new-message',
+  label: {
+    text: 'Сообщение',
+  },
+  validation: {
+    required: true,
+  },
+});
+
+const submitButton = new Button({
+  className: 'main-page__submit-button',
+  text: ' ',
+  type: BUTTON_TYPE.SUBMIT,
+});
+
+const newMessageForm = new Form({
+  className: 'main-page__send-message',
+  children: [newMessageInput, submitButton],
+  events: {
+    submit: {
+      callback: (evt) => onFormSubmit(evt),
+    },
+    focus: {
+      callback: (evt) => {
+        const target = evt.target as HTMLInputElement;
+
+        if (!target.checkValidity()) {
+          target.reportValidity();
+        }
+      },
+      capture: true,
+    },
+  },
+});
+
+class MainPage extends Block {
+  constructor(props: Props) {
+    super(TAG_NAME.DIV, props);
+  }
+
+  render() {
+    return this.compile(template, this.props);
+  }
+}
+
+const allProps = {
+  addChatLink,
+  profileLink,
+  chatsList,
+  messagesList,
+  form: newMessageForm,
+};
+
+const page = new MainPage(allProps);
+
+const root = document.getElementById('root');
+
+renderDOM(root, page);
