ae97c87b5ac2df6913f16a63f91ee773715a2721
Alex Morozov <alex.morozov.webdev@gmail.com>
2022-04-10 15:25:47 +0300

    create form component

---
+++ src/components/form/form.ts
@@ -0,0 +1,19 @@
+import {template} from './form.tmpl';
+import {Block} from '../block';
+import {TAG_NAME} from '../../constants';
+
+type Props = {
+  className: string;
+  children: Block[];
+  events?: Record<string, EventProps>;
+};
+
+export class Form extends Block {
+  constructor(props: Props) {
+    super(TAG_NAME.FORM, props);
+  }
+
+  render() {
+    return this.compile(template, this.props);
+  }
+}
