def3219e06ad419f503577cf870be47fd425cc95
Alex Morozov <alex.morozov.webdev@gmail.com>
2022-05-10 21:21:22 +0300

    created router component

---
+++ src/core/router/router.ts
@@ -0,0 +1,87 @@
+import {Block} from '..';
+import {Route} from '../route';
+
+export class Router {
+  private static instance: Router;
+
+  private readonly _root: string;
+
+  private _currentRoute: Route | null;
+
+  history: History;
+
+  routes: Route[];
+
+  private constructor(root: string) {
+    this.routes = [];
+    this.history = window.history;
+    this._currentRoute = null;
+    this._root = root;
+  }
+
+  public static getInstance(root: string) {
+    if (!Router.instance) {
+      Router.instance = new Router(root);
+    }
+
+    return Router.instance;
+  }
+
+  private _onRoute(path: string) {
+    const route = this.getRoute(path);
+
+    if (!route) {
+      return;
+    }
+
+    if (this._currentRoute && !this._currentRoute.match(path)) {
+      this._currentRoute.leave();
+    }
+
+    if (route) {
+      this._currentRoute = route;
+      route.render();
+    }
+  }
+
+  use<T extends Block, P extends CommonProps>(
+    path: string,
+    component: new (props?: any) => T,
+    componentProps?: P,
+  ) {
+    const route = new Route(path, component, {...componentProps, root: this._root});
+    this.routes.push(route);
+    return this;
+  }
+
+  start() {
+    window.onpopstate = (evt) => this._onRoute((evt.currentTarget as Window).location.pathname);
+    this._onRoute(window.location.pathname);
+  }
+
+  go(path: string | number) {
+    if (typeof path === 'number') {
+      if (path === 1) {
+        this.forward();
+      }
+      if (path === -1) {
+        this.back();
+      }
+    } else {
+      this.history.pushState({}, '', path);
+      this._onRoute(path);
+    }
+  }
+
+  forward() {
+    this.history.forward();
+  }
+
+  back() {
+    this.history.back();
+  }
+
+  getRoute(path: string) {
+    return this.routes.find((route) => route.match(path));
+  }
+}
