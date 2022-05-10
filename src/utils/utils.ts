5aba47e0de60c7a89749d5dc7589115721ef15a3
Alex Morozov <alex.morozov.webdev@gmail.com>
2022-05-10 17:42:27 +0300

    added predicates

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -32,6 +32,30 @@ export const get = <T extends Record<string, unknown>, D>(
 export const isObject = (value: unknown): value is Record<string, unknown> =>
   Boolean(value) && typeof value === 'object' && !Array.isArray(value);
 
+/**
+ * Предикат на тип Function
+ *
+ * @param value - определяемое значение
+ */
+
+export const isFunction = (value: unknown): value is Function =>
+  Boolean(value) && typeof value === 'function';
+
+/**
+ * Предикат на тип Array
+ *
+ * @param value - определяемое значение
+ */
+
+export const isArray = (value: unknown): value is [] => Boolean(value) && Array.isArray(value);
+
+/**
+ * Функция для глубокого копирования объекта или массива
+ *
+ * @deprecated - переписать реализацию для объектов и массивов
+ * @param object - клонируемый объект
+ */
+
 export const cloneObject = <T>(object: T): T => {
   if (!object) {
     throw new Error('Object not found');
