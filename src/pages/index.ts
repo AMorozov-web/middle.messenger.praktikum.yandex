fd4ac43b2cb7b9b846d5141e053aa551473d23ea
Alex Morozov <alex.morozov.webdev@gmail.com>
2022-05-11 00:37:10 +0300

    created pages index

---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+export * from './main';
+export * from './error';
+export * from './login';
+export * from './change-user-data';
+export * from './change-user-password';
+export * from './sign-up';
+export * from './profile';
