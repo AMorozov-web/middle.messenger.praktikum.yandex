97c9c4a522df843ca8bfd0a7b031be845de164ab
Alex Morozov <alex.morozov.webdev@gmail.com>
2022-06-14 20:22:58 +0300

    refactored connect function

--- src/store/connect.ts
+++ src/store/connect.ts
@@ -11,18 +11,19 @@ const getProps = <T extends CommonProps>(props: T, mappedProps?: T): T => {
 
 export const connect = <P extends CommonProps>(
   Component: new (props: P) => Block<P>,
-  mapStateToProps?: (state: RootState) => P,
+  mapStateToProps: (state: RootState) => P,
 ) => {
   return class extends Component {
     constructor(props: P) {
-      const newProps = getProps<P>(props, mapStateToProps?.(store.getState()));
+      const newProps = getProps<P>(props, mapStateToProps(store.getState()));
       super(newProps);
 
-      this.setProps(newProps);
+      // this.setProps(newProps); // возможно нужно убрать
 
       store.on(STORE_EVENTS.UPDATED, () => {
-        const mappedProps = mapStateToProps?.(store.getState());
-        this.setProps(mappedProps ?? ({} as P));
+        const mappedProps = mapStateToProps(store.getState());
+
+        this.setProps(mappedProps);
       });
     }
   };
