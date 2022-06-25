04de1a0c3b5ddc88c1eeef48e07ad6fa0c6d361e
Alex Morozov <alex.morozov.webdev@gmail.com>
2022-06-25 22:27:57 +0300

    removed parcel, installed webpack, docker and husky, configured environment

--- server/express.js
+++ server/express.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 
 const DEFAULT_PORT = 3000;
 
@@ -6,6 +6,6 @@ const port = process.env.PORT || DEFAULT_PORT;
 
 const app = express();
 
-app.use(express.static("dist"));
+app.use(express.static('./dist'));
 
-app.listen(port, () => console.log(`Server working on Port ${port}`));
+app.listen(port);
